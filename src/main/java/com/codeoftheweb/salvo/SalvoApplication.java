93ed42c1aa2f0e7751d64376dcc63c90a2de21ec
JonatanQuiles87 <108682518+JonatanQuiles87@users.noreply.github.com>
2023-09-28 23:40:00 +0200

    Adding Controller.java and Sample Players with @Bean

--- src/main/java/com/codeoftheweb/salvo/SalvoApplication.java
+++ src/main/java/com/codeoftheweb/salvo/SalvoApplication.java
@@ -1,13 +1,26 @@
 package com.codeoftheweb.salvo;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SalvoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SalvoApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner initData(PlayerRepository repository) {
+		return (args) -> {
+			// save a couple of customers
+			repository.save(new Player("partymix69a@hotmail.com"));
+			repository.save(new Player("jonathan.quiles@outlook.es"));
+			repository.save(new Player("jessibcn@gmail.com"));
+			repository.save(new Player("oriol.furnells@gmail.com"));
+		};
 	}
 
 }
