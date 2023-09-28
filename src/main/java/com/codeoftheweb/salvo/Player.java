065c822775fbf0eccf2a73a2f29bd1557109008f
JonatanQuiles87 <108682518+JonatanQuiles87@users.noreply.github.com>
2023-09-28 20:40:35 +0200

    First Commit

---
+++ src/main/java/com/codeoftheweb/salvo/Player.java
@@ -0,0 +1,33 @@
+package com.codeoftheweb.salvo;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Player {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO, generator = "native")
+    private long id;
+
+    private String userName;
+
+    public Player() {
+    }
+    public Player (String userName) {
+        this.userName = userName;
+    }
+    public long getId (){
+        return id;
+    }
+    public void setId (long id) {
+        this.id = id;
+    }
+    public String getUserName() {
+        return userName;
+    }
+    public void setUserName (String userName) {
+        this.userName = userName;
+    }
+}
