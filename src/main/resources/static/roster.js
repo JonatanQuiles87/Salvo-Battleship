cc4532bd5dcaf0ffcf006358a015c460816ff8a5
JonatanQuiles87 <108682518+JonatanQuiles87@users.noreply.github.com>
2023-10-06 23:34:05 +0200

    Front-end Player class

---
+++ src/main/resources/static/roster.js
@@ -0,0 +1,47 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const playerList = document.getElementById("player-list");
+    const addPlayerButton = document.getElementById("add-player");
+    const newPlayerInput = document.getElementById("new-player");
+
+    // Function to fetch and display players
+    function fetchPlayers() {
+        fetch("/players")
+            .then((response) => response.json())
+            .then((data) => {
+                playerList.innerHTML = ""; // Clear existing list
+                data._embedded.players.forEach((player) => {
+                    const li = document.createElement("li");
+                    li.textContent = player.userName;
+                    playerList.appendChild(li);
+                });
+            })
+            .catch((error) => {
+                console.error("Error fetching players:", error);
+            });
+    }
+
+    // Event listener for Add button
+    addPlayerButton.addEventListener("click", function () {
+        const newPlayerName = newPlayerInput.value;
+        if (newPlayerName) {
+            // POST new player to /players
+            fetch("/players", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({ userName: newPlayerName }),
+            })
+                .then(() => {
+                    // After successful POST, fetch and display players again
+                    fetchPlayers();
+                })
+                .catch((error) => {
+                    console.error("Error adding player:", error);
+                });
+        }
+    });
+
+    // Initial fetch and display of players
+    fetchPlayers();
+});
