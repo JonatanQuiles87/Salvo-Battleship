399603fd94a4d2f84a49fadb988f35b1418cc59a
JonatanQuiles87 <108682518+JonatanQuiles87@users.noreply.github.com>
2023-10-12 20:22:21 +0200

    Rename Controller

--- src/main/java/com/codeoftheweb/salvo/SalvoController.java
+++ src/main/java/com/codeoftheweb/salvo/SalvoController.java
@@ -3,8 +3,10 @@ package com.codeoftheweb.salvo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
-public class Controller {
+public class SalvoController {
     @Autowired
-    private PlayerRepository playerRepository;
+    private PlayerRepository playerRepository ;
 }
