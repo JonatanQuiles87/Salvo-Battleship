dbe56f76ac4afeb7c8818ecfa66f74b27db2dd62
JonatanQuiles87 <108682518+JonatanQuiles87@users.noreply.github.com>
2024-01-16 14:41:38 +0100

    .

--- src/main/java/com/codeoftheweb/salvo/repositories/PlayerRepository.java
+++ src/main/java/com/codeoftheweb/salvo/repositories/PlayerRepository.java
@@ -4,10 +4,7 @@ import com.codeoftheweb.salvo.model.Player;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
 
-import java.util.Optional;
-
 public interface PlayerRepository extends JpaRepository<Player, Long> {
     Player findByUserName(@Param("userName") String userName);
 
-    Optional<Player> findById (Long personId);
 }
