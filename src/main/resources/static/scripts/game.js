a9548f4b3207b1734e5634bdda1c66fa9c660521
JonatanQuiles87 <108682518+JonatanQuiles87@users.noreply.github.com>
2024-02-12 14:41:48 +0100

    - Remove and Save buttons have been added.
    EventListener added to remove button.
    -Selected ship is removed from the map.

--- src/main/resources/static/scripts/game.js
+++ src/main/resources/static/scripts/game.js
@@ -5,19 +5,20 @@ import {logout} from "./utilities/authorization.js";
 import {allShipTypes} from "./utilities/constants.js";
 
 const gridSize = 10;
-const lastLetterInMap = String.fromCharCode(65 + gridSize - 1); // We need 1 because charcode return array is also zero indexed.
+const lastLetterInMap = String.fromCharCode(65 + gridSize - 1); // Because charcode return array is also zero indexed, we need to subtract 1.
 const shipsGridContainer = document.querySelector('#ships-grid-container');
 const salvoesGridContainer = document.querySelector('#salvoes-grid-container');
-const loggedInPlayerUsernameArea = document.querySelector('logged-in-player');
+const loggedInPlayerUsernameArea = document.querySelector('#logged-in-player');
 const logoutBtn = document.querySelector('#logout-btn');
-const placedShipsContainer = querySelector('.placed-ships-container');
+const placedShipsContainer = document.querySelector('.placed-ships-container');
+const removeShipButton = document.querySelector('#removeShip');
+const saveShipButton = document.querySelector('#saveShip');
 
 shipsGridContainer.setAttribute('style', `grid-template-columns:repeat(${gridSize + 1}, 1fr)`); // To be able to have dynamic grid size in case we want different size of grid.
 salvoesGridContainer.setAttribute('style', `grid-template-columns:repeat(${gridSize + 1}, 1fr)`);
 let rowLetterShip = 'A'; // The beginning letter of the row.
 let rowLetterSalvo = 'A';
 let shipObjectListPlacedByUser = [];
-console.log('shipObjectListPlacedByUser', shipObjectListPlacedByUser);
 
 const params = new Proxy(new URLSearchParams(window.location.search), {
     get: (searchParams, prop) => searchParams.get(prop),
@@ -25,7 +26,6 @@ const params = new Proxy(new URLSearchParams(window.location.search), {
 const gamePlayerId = params['gp'];
 const fetchedGameView = await fetchGameViewObject(gamePlayerId);
 const allLocationsOfAlreadySentOwnerShips = combineShipsLocations(fetchedGameView['ships']);
-console.log('allLocationsOfAlreadySentOwnerShips', allLocationsOfAlreadySentOwnerShips);
 
 if (loggedInPlayerUsername) {
     showPlayerUsername(loggedInPlayerUsername, loggedInPlayerUsernameArea);
@@ -60,7 +60,7 @@ function createColumnHeaders(gridContainer) {
     }
 }
 
-function createRowCells(gridContainer, rowLetter) {
+function createRowCells(gridContainer, rowLetter, gridItemCallBack) {
     for (let columnNo = 0; columnNo <= gridSize; columnNo++) {
         const gridItem = document.createElement('label');
         gridItem.setAttribute('class', 'grid-item');
@@ -82,11 +82,126 @@ function createRowCells(gridContainer, rowLetter) {
         rowLetterSalvo = nextChar(rowLetterSalvo);
 }
 
+function placeDataOnGrids() {
+    if (gamePlayerId !== null) {
+        placeAlreadySavedShipsOnGrid(fetchedGameView['ships']);
+        showGameInfo(fetchedGameView['gamePlayers']);
+        placeSalvoesOnGrids(fetchedGameView);
+        createShipsForms(fetchedGameView);
+    }
+}
+
+function placeAlreadySavedShipsOnGrid(ships) {
+    ships.forEach(ship => {
+        ship['shipLocations'].forEach(location => {
+            const gridCell = document.querySelector(`#SHIP${location}`);
+            gridCell ?
+                gridCell.setAttribute('style', 'background-color: darkblue') :
+                alert(`The location ${location} is not exist in the grid table. Check your locations.`);
+        });
+    });
+}
+
+function showGameInfo(gamePlayers) {
+    const gamePlayerOwner = gamePlayers.find(({id}) => id.toString() === gamePlayerId);
+    const ownerUsername = gamePlayerOwner['player']['username'];
+    const gamePlayerOpponent = gamePlayers.find(({id}) => id.toString() !== gamePlayerId);
+    const opponentUsername = gamePlayerOpponent === undefined ? '"waiting_for_opponent"' : gamePlayerOpponent['player']['username'];
+
+    const gameInfoTextField = document.querySelector('#game-info');
+    const gameInfoText = document.createTextNode(`${ownerUsername} (you) vs ${opponentUsername}`);
+    gameInfoTextField.innerHTML = '';
+    gameInfoTextField.appendChild(gameInfoText);
+}
+
+function placeSalvoesOnGrids(game) {
+    const gamePlayerOwner = game['gamePlayers'].find(({id}) => id.toString() === gamePlayerId);
+    const ownerPlayerId = gamePlayerOwner['player']['id'];
+    placeOwnerSalvoes(game['salvoes'][ownerPlayerId]);
+
+    const gamePlayerOpponent = game['gamePlayers'].find(({id}) => id.toString() !== gamePlayerId);
+    if (gamePlayerOpponent !== undefined) {
+        const opponentPlayerId = gamePlayerOpponent['player']['id'];
+        placeOpponentSalvoes(game['salvoes'][opponentPlayerId]);
+    }
+}
+
+function placeOwnerSalvoes(ownerSalvoes) {
+    const entriesOwnerSalvoes = Object.entries(ownerSalvoes);
+    entriesOwnerSalvoes.forEach(ownerSalvo => {
+        const turnNumber = ownerSalvo[0];
+        const salvoLocations = ownerSalvo[1];
+        salvoLocations.forEach(location => {
+            const gridCellInSalvoGrid = document.querySelector(`#SALVO${location}`);
+            gridCellInSalvoGrid.setAttribute('style', 'background-color: darkred ; color: white');
+            gridCellInSalvoGrid.innerHTML = turnNumber;
+        });
+    });
+}
+
+function placeOpponentSalvoes(opponentSalvoes) {
+    const entriesOpponentSalvoes = Object.entries(opponentSalvoes);
+    entriesOpponentSalvoes.forEach(opponentSalvo => {
+        const turnNumber = opponentSalvo[0];
+        const salvoLocations = opponentSalvo[1];
+        salvoLocations.forEach(location => {
+            const gridCellInOwnerShipGrid = document.querySelector(`#SHIP${location}`);
+            gridCellInOwnerShipGrid.innerHTML = turnNumber;
+            if (allLocationsOfAlreadySentOwnerShips.includes(location)) {
+                gridCellInOwnerShipGrid.setAttribute('style', 'background-color: purple ; color: white');
+            } else {
+                gridCellInOwnerShipGrid.setAttribute('style', 'background-color: darkred ; color: white');
+            }
+        });
+    });
+}
+
+function createShipsForms(game) {
+    const shipsToPlaceContainer = document.querySelector('.ships-to-place-container');
+
+    const shipsAlreadySentInGame = game['ships']
+        .map(({shipType}) => shipType.toLowerCase());
+
+    allShipTypes.forEach(ship => {
+        const container = document.createElement('div');
+        container.classList.add('form-check');
+
+        const input = createInput(ship, 'radio');
+        const label = createLabel(ship, 'radio');
+
+        if (shipsAlreadySentInGame.includes(ship.name.toLowerCase())) {
+            input.disabled = true;
+            label.setAttribute('style', 'text-decoration: line-through red');
+        }
+
+        container.appendChild(input);
+        container.appendChild(label);
+        shipsToPlaceContainer.appendChild(container);
+    });
+}
+
+function createInput(selectedShipData, type) {
+    const input = document.createElement('input');
+    input.classList.add('form-check-input');
+    input.setAttribute('type', type);
+    input.setAttribute('id', `${selectedShipData.id}_${type}`);
+    if (type === 'radio') {
+        input.setAttribute('name', 'shipsToPlace');
+    }
+    return input;
+}
+
+function createLabel(selectedShipData, type) {
+    const label = document.createElement('label');
+    label.classList.add('form-check-label');
+    label.setAttribute('for', `${selectedShipData.id}_${type}`);
+    label.textContent = `${selectedShipData.name} (${'\u25A0 '.repeat(selectedShipData.size)})`;
+    return label;
+}
+
 function handleShipGridItemClick(event) {
     const clickedItemId = event.target.id;
     const clickedItemGridCode = clickedItemId.slice(4);
-    console.log('clickedItemId', clickedItemId);
-    console.log('clickedItemGridCode', clickedItemGridCode);
     const selectedShip = getSelectedShip();
     const selectedDirection = getSelectedDirection();
     if (!selectedShip || !selectedDirection) {
@@ -99,15 +214,10 @@ function handleShipGridItemClick(event) {
         const isThereEnoughSpace = checkIfThereIsEnoughSpace(shipSize, selectedDirection, clickedItemGridCode);
         if (isThereEnoughSpace) {
             const shipObject = createShipObject(shipName, shipSize, selectedDirection, clickedItemGridCode);
-            console.log('shipObject', shipObject);
             placeSelectedShipOnGrid(shipObject, selectedShipData);
         } else {
             alert('Ship cannot be placed.');
         }
-        console.log('selectedShipData', selectedShipData);
-        console.log('selectedShip', selectedShip);
-        console.log('shipSize', shipSize);
-        console.log('selectedDirection', selectedDirection);
     }
 }
 
@@ -174,165 +284,103 @@ function getSelectedDirection() {
     return checkedShipDirection;
 }
 
-function placeSelectedShipOnGrid(ship, selectedShipData) {
+function placeSelectedShipOnGrid(shipObject, selectedShipData) {
     const allLocationsOfPlacedShips = combineShipsLocations(shipObjectListPlacedByUser);
-    console.log('allLocationsOfPlacedShips', allLocationsOfPlacedShips);
     const combinedSavedAndPlacedShipLocations = [...allLocationsOfAlreadySentOwnerShips, ...allLocationsOfPlacedShips];
-    console.log('combinedSavedAndPlacedShipLocations', combinedSavedAndPlacedShipLocations);
-    const isOverLap = isShipOverLap(ship, combinedSavedAndPlacedShipLocations);
-    if(isOverLap) {
+    const isOverlap = isShipOverlap(shipObject, combinedSavedAndPlacedShipLocations);
+    if (isOverlap) {
         alert('Ships are overlapping, select another point.');
     } else {
-        ship['shipLocations'].forEach(location => {
+        shipObject['shipLocations'].forEach(location => {
             const gridCell = document.querySelector(`#SHIP${location}`);
             gridCell ?
-                gridCell.setAttribute('style', 'background-color: lightblue') :
+                gridCell.setAttribute('style', 'background-color: lightskyblue') :
                 alert(`The location ${location} is not exist in the grid table. Check your locations.`);
         });
-        shipObjectListPlacedByUser.push(ship);
+        shipObjectListPlacedByUser.push(shipObject);
         createPlacedShipsCheckBoxes(selectedShipData);
     }
 }
 
-function isOverLap(ship, combinedSavedAndPlacedShipLocations) {
-    const selectedShipLocations = ship['shipLocations'];
+function isShipOverlap(shipObject, combinedSavedAndPlacedShipLocations) {
+    const selectedShipLocations = shipObject['shipLocations'];
     const allLocationsSet = new Set([...selectedShipLocations, ...combinedSavedAndPlacedShipLocations]);
     return allLocationsSet.size < selectedShipLocations.length + combinedSavedAndPlacedShipLocations.length;
 }
 
-function handleSalvoGridItemClick() {
-    console.log('Salvo grid item click handled');
-}
-
-function placeDataOnGrids() {
-    if (gamePlayerId !== null) {
-        placeAlreadySavedShipsOnGrid(fetchedGameView['ships']);
-        showGameInfo(fetchedGameView['gamePlayers']);
-        placeSalvoesOnGrids(fetchedGameView);
-        createShipsForms(fetchedGameView);
-    }
-}
-
-function placeAlreadySavedShipsOnGrid(ships) {
-    ships.forEach(ship => {
-        ship['shipLocations'].forEach(location => {
-            const gridCell = document.querySelector(`#SHIP${location}`);
-            gridCell ?
-                gridCell.setAttribute('style', 'background-color: darkblue') :
-                alert(`The location ${location} is not exist in the grid table. Check your locations.`);
-        });
-    });
-}
-
-function showGameInfo(gamePlayers) {
-    const gamePlayerOwner = gamePlayers.find(({id}) => id.toString() === gamePlayerId);
-    const ownerUsername = gamePlayerOwner['player']['username'];
-    const gamePlayerOpponent = gamePlayers.find(({id}) => id.toString() !== gamePlayerId);
-    const opponentUsername = gamePlayerOpponent === undefined ? '"waiting_for_opponent"' : gamePlayerOpponent['player']['username'];
-
-    const gameInfoTextField = document.querySelector('#game-info');
-    const gameInfoText = document.createTextNode(`${ownerUsername} (you) vs ${opponentUsername}`);
-    gameInfoTextField.innerHTML = '';
-    gameInfoTextField.appendChild(gameInfoText);
-}
-
-function placeSalvoesOnGrids(game) {
-    const gamePlayerOwner = game['gamePlayers'].find(({id}) => id.toString() === gamePlayerId);
-    const ownerPlayerId = gamePlayerOwner['player']['id'];
-    placeOwnerSalvoes(game['salvoes'][ownerPlayerId]);
-
-    const gamePlayerOpponent = game['gamePlayers'].find(({id}) => id.toString() !== gamePlayerId);
-    if (gamePlayerOpponent !== undefined) {
-        const opponentPlayerId = gamePlayerOpponent['player']['id'];
-        placeOpponentSalvoes(game['salvoes'][opponentPlayerId]);
-    }
-}
-
-function placeOwnerSalvoes(ownerSalvoes) {
-    const entriesOwnerSalvoes = Object.entries(ownerSalvoes);
-    entriesOwnerSalvoes.forEach(ownerSalvo => {
-        const turnNumber = ownerSalvo[0];
-        const salvoLocations = ownerSalvo[1];
-        salvoLocations.forEach(location => {
-            const gridCellInSalvoGrid = document.querySelector(`#SALVO${location}`);
-            gridCellInSalvoGrid.setAttribute('style', 'background-color: darkred ; color: white');
-            gridCellInSalvoGrid.innerHTML = turnNumber;
-        });
-    });
-}
-
-function placeOpponentSalvoes(opponentSalvoes) {
-    const ownerShipsAllLocations = combineOwnerShipsLocations(ownerShips);
-
-    const entriesOpponentSalvoes = Object.entries(opponentSalvoes);
-    entriesOpponentSalvoes.forEach(opponentSalvo => {
-        const turnNumber = opponentSalvo[0];
-        const salvoLocations = opponentSalvo[1];
-        salvoLocations.forEach(location => {
-            const gridCellInOwnerShipGrid = document.querySelector(`#SHIP${location}`);
-            gridCellInOwnerShipGrid.innerHTML = turnNumber;
-            if (allLocationsOfAlreadySentOwnerShips.includes(location)) {
-                gridCellInOwnerShipGrid.setAttribute('style', 'background-color: purple ; color: white');
-            } else {
-                gridCellInOwnerShipGrid.setAttribute('style', 'background-color: darkred ; color: white');
-            }
-        });
-    });
-}
-
-function createShipsForms(game) {
-    const shipsToPlaceContainer = document.querySelector('.ships-to-place-container');
-
-    const shipsAlreadySentInGame = game['ships']
-        .map(({shipType}) => shipType.toLowerCase());
-
-    allShipTypes.forEach(ship => {
-        const container = document.createElement('div');
-        container.classList.add('form-check');
-
-        const input = createInput(ship, 'radio');
-        const label = createLabel(ship, 'radio');
-
-        if (shipsAlreadySentInGame.includes(ship.name.toLowerCase())) {
-            input.disabled = true;
-            label.setAttribute('style', 'text-decoration: line-through red');
-        }
-
-        container.appendChild(input);
-        container.appendChild(label);
-        shipsToPlaceContainer.appendChild(container);
-    });
-}
-
-function createInput(ship, type) {
-    const input = document.createElement('input');
-    input.classList.add('form-check-input');
-    input.setAttribute('type', type);
-    input.setAttribute('id', ship.id);
-    if (type === 'radio') {
-        input.setAttribute('name', 'shipsToPlace');
-    }
-    return input;
-}
-
-function createLabel(ship, type) {
-    const label = document.createElement('label');
-    label.classList.add('form-check-label');
-    label.setAttribute('for', `${ship.id}_${type}`);
-    label.textContent = `${ship.name} (${'\u25A0 '.repeat(ship.size)})`;
-    return label;
-}
-
-function createPlacedShipsCheckBoxes(ship) {
+function createPlacedShipsCheckBoxes(selectedShipData) {
     const container = document.createElement('div');
     container.classList.add('form-check');
-    const input = createInput(ship, 'checkbox');
-    const label = createLabel(ship, 'checkbox');
-    const placedShipRadioButton = document.querySelector(`#${ship.id}_radio`);
-    placedShipRadioButton.checked = false;
-    placedShipRadioButton.disabled = true;
-
+    const input = createInput(selectedShipData, 'checkbox');
+    const label = createLabel(selectedShipData, 'checkbox');
+    input.addEventListener('click', updateRemoveAndSaveButtons);
     container.appendChild(input);
     container.appendChild(label);
     placedShipsContainer.appendChild(container);
+
+    const placedShipRadioButton = document.querySelector(`#${selectedShipData.id}_radio`);
+    placedShipRadioButton.checked = false;
+    placedShipRadioButton.disabled = true;
+}
+
+function updateRemoveAndSaveButtons() {
+    const checkboxes = placedShipsContainer.querySelectorAll('input[type="checkbox"]');
+    const isAnyShipChecked = Array.from(checkboxes).some(checkbox => checkbox.checked);
+    removeShipButton.disabled = !isAnyShipChecked;
+    saveShipButton.disabled = !isAnyShipChecked;
+}
+
+removeShipButton.addEventListener('click', removeCheckedShip);
+
+function removeCheckedShip() {
+    const checkedShipIds = getCheckedShipIds();
+    checkedShipIds.forEach(shipId => {
+        removeShipFromCheckboxShipList(shipId);
+        enableShipOnRadioShipList(shipId);
+        removeShip(shipId);
+        updateRemoveAndSaveButtons();
+    });
+}
+
+function getCheckedShipIds() {
+    const checkboxes = placedShipsContainer.querySelectorAll('input[type="checkbox"]');
+    return Array.from(checkboxes)
+        .filter(checkbox => checkbox.checked)
+        .map(checkbox => checkbox.id.split('_')[0]);
+}
+
+function removeShipFromCheckboxShipList(shipId) {
+    const checkedShip = document.getElementById(shipId + '_checkbox');
+    const parentElementOfCheckedShip = checkedShip.closest('.form-check');
+    parentElementOfCheckedShip.remove();
+}
+
+function enableShipOnRadioShipList(shipId) {
+    const shipRadioButton = document.getElementById(shipId + '_radio');
+    shipRadioButton.disabled = false;
+}
+
+function removeShip(shipId) {
+    const shipName = allShipTypes
+        .find(ship => ship.id === shipId).name;
+    const shipObjectToRemove = shipObjectListPlacedByUser.find(shipObject => shipObject['shipType'] === shipName);
+    const locationsShipObjectToRemove = shipObjectToRemove['shipLocations'];
+    deleteShipFromMap(locationsShipObjectToRemove);
+
+    const indexToRemove = shipObjectListPlacedByUser.findIndex(shipObject => shipObject['shipType'] === shipName);
+    if(indexToRemove !== -1){
+        shipObjectListPlacedByUser.splice(indexToRemove, 1);
+    }
+}
+
+function deleteShipFromMap(locationsShipObjectToRemove){
+    locationsShipObjectToRemove.forEach(location => {
+        const gridCellId = 'SHIP' + location;
+        const gridCell = document.getElementById(gridCellId);
+        gridCell.setAttribute('style', 'background-color: lightgray');
+    });
+}
+
+function handleSalvoGridItemClick() {
+    console.log('Salvo grid item click handled');
 }
